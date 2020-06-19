f8266d07055b68b3de1eca68652b4e6cbfb7d223
christophermarek <christophermarek42@gmail.com>
2020-06-18 23:47:54 -0400

    Initial skeleton created

---
+++ life/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [boardSize, setSize] = useState(10);
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
