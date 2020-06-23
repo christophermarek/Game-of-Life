41cd70c337303c857d399682468cefdf915c6da9
christophermarek <christophermarek42@gmail.com>
2020-06-22 23:36:33 -0400

    Added board parsing, need to fix logic

--- life/src/App.js
+++ life/src/App.js
@@ -6,6 +6,18 @@ function App() {
   const [board, setBoard] = useState([]);
   const [started, setStarted] = useState(false);
 
+  useEffect(() => {
+    let interval = null;
+    if (started) {
+      interval = setInterval(() => {
+        simulationLoop();
+      }, 1000);
+    } else if (!started) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [started]);
+
   //generateBoard from boardSize args
   function generateBoard(e){
     e.preventDefault();
@@ -33,16 +45,110 @@ function App() {
     let newBoard = [...board];
     newBoard[x][y].active = !newBoard[x][y].active;
     setBoard(board => (newBoard));
-    console.log(board);
   }
 
   function clickCell(x, y){
     updateBoard(x, y);
   }
 
+
+  //returns count of neighbours to cell
+  function countNeighbours(x,y){
+    console.log("############");
+    let count = 0;
+    //find a more efficient way to iterate
+
+    //col1
+    //out of first array bounds
+    if(x - 1 >= 0){
+      if(y - 1 >= 0){
+        count = (board[x-1][y-1].active) ? count + 1 : count;
+      }
+      count = (board[x-1][y].active) ? count + 1 : count;
+      if(y + 1 <= boardSize - 1){
+        count = (board[x-1][y+1].active) ? count + 1 : count;
+      }
+    }
+    console.log(count);
+
+    //col2
+    if(y - 1 >= 0){
+      count = (board[x][y-1].active) ? count + 1 : count;
+    }
+    if(y + 1 <= boardSize - 1){
+      count = (board[x][y+1]) ? count + 1 : count;
+    }
+    /*
+    why is count 2???
+    for 0,0 count increments at col 2 and 3 when it should only on 3 at [x+1][y]
+    */
+    console.log(count);
+    //col3
+    //check second array bounds
+    if(x + 1 <= boardSize - 1){
+      if(y - 1 >= 0){
+        count = (board[x+1][y-1].active) ? count + 1 : count;
+      }
+      count = (board[x+1][y].active) ? count + 1 : count;
+      if(y + 1 <= boardSize - 1){
+        count = (board[x+1][y+1].active) ? count + 1 : count;
+      }
+    }
+    console.log(count);
+    if(count === 2){
+      console.log(x + " : " + y);
+    }
+    console.log("--------------")
+    return count;
+  }
+
+
+  function parseBoard(){
+    let count = 0;
+    for(let n = 0; n < boardSize; n++){
+      for(let m = 0; m < boardSize; m++){
+        count = countNeighbours(board[n][m].x, board[n][m].y);
+        updateCell(count, board[n][m].x, board[n][m].y);
+      }
+    }
+
+  }
+
+  function rule1(){
+    //1.Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+  }
+  function rule2(){
+    //2.Any live cell with two or three live neighbours lives on to the next generation.
+  }
+  
+  function rule3(){
+    //3.Any live cell with more than three live neighbours dies, as if by overpopulation.
+  }
+  
+  function rule4(){
+    //4.Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  }
+  
+  function updateCell(numNeighbours, x, y){
+    //console.log(numNeighbours);
+    rule1();
+    rule2();
+    rule3();
+    rule4();
+
+  }
+  
+  function simulationLoop(){
+    parseBoard();
+  }
+
   function startStimulation(){
     //check if board generated
     setStarted(true);
+  }
+
+  function pauseSimulation(){
+    setStarted(false);
   }
   
 
@@ -57,7 +163,10 @@ function App() {
           <button onClick={startStimulation}>Start Simulation</button>
         </>
       ) : (
-        <p>Started</p>
+        <>
+          <p>Started</p>
+          <button onClick={pauseSimulation}>Pause Simulation</button>
+        </>
       )}
       <div className="boardContainer">
         {board.map(function (item, i){
