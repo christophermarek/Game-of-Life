5e0e52cc08dc277446058a61d75d9a8b9b613a40
christophermarek <christophermarek42@gmail.com>
2020-06-22 21:44:35 -0400

    Board ready before rules

--- life/src/App.js
+++ life/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -10,14 +10,17 @@ function App() {
   function generateBoard(e){
     e.preventDefault();
     //add form input for size later
-    const defaultTile = {
-      active:false,
-    };
+    
     let newBoard = [];
     
     for(let i = 0; i < boardSize; i++){
       let columns = [];
       for(let j = 0; j < boardSize; j++){
+        let defaultTile = {
+          active:false,
+          x: i,
+          y: j,
+        };
         columns.push(defaultTile);
       }
       newBoard.push(columns);  
@@ -26,39 +29,52 @@ function App() {
     setBoard(board => (newBoard));
   };
 
-  function clickCell(e){
-    
+  function updateBoard(x, y){
+    let newBoard = [...board];
+    newBoard[x][y].active = !newBoard[x][y].active;
+    setBoard(board => (newBoard));
+    console.log(board);
   }
 
+  function clickCell(x, y){
+    updateBoard(x, y);
+  }
+
+  function startStimulation(){
+    //check if board generated
+    setStarted(true);
+  }
   
-  let rows = board.map(function (item, i){
-    let entry = item.map(function (element, j) {
-        return ( 
-            <div 
-              className="cellContainer" 
-              key={j} 
-              onClick={clickCell}
-              style={{backgroundColor:element.active? "black" : "white"}}
-              ></div>
-            );
-    });
-    return (
-        <div className="row" key={i}>{entry}</div>
-     );
-  });
 
   return (
     <div className="App">
       <form onSubmit={generateBoard} >
         <button type="Submit">Generate</button>
       </form>
-      {started ? (
-        <p>Not Started</p>
+      {!started ? (
+        <>
+          <p>Not Started</p>
+          <button onClick={startStimulation}>Start Simulation</button>
+        </>
       ) : (
         <p>Started</p>
       )}
       <div className="boardContainer">
-        {rows}
+        {board.map(function (item, i){
+          let entry = item.map(function (element, j) {
+              return ( 
+                  <div 
+                    className="cellContainer" 
+                    key={j}
+                    onClick={() => clickCell(element.x, element.y)}
+                    style={{backgroundColor:element.active? "black" : "white"}}
+                    ></div>
+                  );
+          });
+          return (
+              <div className="row" key={i}>{entry}</div>
+          );
+        })}
       </div>
 
       
